568f063ca4690b613b4de1459bdfa4180921300d
Paul Ambrose <pablo_rose34@hotmail.com>
2022-11-16 20:16:43 +0000

    Add files via upload

--- SensHatTempLogger.py
+++ SensHatTempLogger.py
@@ -1,36 +1,42 @@
 #An object Oriented program using sense HAT that
 #  >>Creates a text file for the day
-#  >>Reads the temperature, pressure and humidity every 5mins across the day and then saves the file
-#  >>Allows the user to select the joy stick button to get a temperature readout in colour banding
+#  >>Reads the temperature, pressure and humidity every 5mins across the day and then
+#    saves the file
 
 from sense_hat import SenseHat
-from time import sleep
+from time import sleep, strftime, time
+import os
+import matplotlib.pyplot as plt
 
 sense = SenseHat()
 sense.clear()
 
+plt.ion()
+x = []
+y = []
+
+path = os.getcwd()
+
 def SensorRun():
-    temp = str(round(sense.get_temperature(),0))  
+    temp = str(round(sense.get_temperature(),0))
     return temp
 
+def write_temp(temp):
+    log = open("log.csv", "a")
+    temp = SensorRun()
+    log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
+    sleep(1)
+    
+def graph(temp):
+    y.append(temp)
+    x.append(time())
+    plt.clf()
+    plt.scatter(x,y)
+    plt.plot(x,y)
+    plt.draw()
+    
 while True:
-
-  temp = SensorRun()
-  myfile = open('log.txt', 'a')
-  myfile.write(temp)
-  myfile.write('\n')
-  myfile.close
-  sleep(10)
-  
-  for event in sense.stick.get_events():
-
-    if event.action == "pressed":
-        temp = SensorRun()
-        
-        if event.direction == "middle":
-            sense.show_message("T " + temp)     
-        else:
-            pass
-        
-        sense.clear()
-        
+    temp = SensorRun()
+    write_temp(temp)
+    graph(temp)
+    plt.pause(1)
